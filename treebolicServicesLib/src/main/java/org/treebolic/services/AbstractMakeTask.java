3d86ea3cd6c2ac2fd2048b252683bd2cdc9f89f0
bbou <bbou@ac-toulouse.fr>
2018-01-16 11:05:43 +0100

    prelint

---
+++ treebolicServicesLib/src/main/java/org/treebolic/services/AbstractMakeTask.java
@@ -0,0 +1,48 @@
+package org.treebolic.services;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import treebolic.model.Model;
+
+/**
+ * Make model task stub
+ */
+abstract class AbstractMakeTask extends AsyncTask<Void, Void, Model>
+{
+	/**
+	 * Log tag
+	 */
+	static private final String TAG = "AMakeTask";
+
+	private final String source;
+	private final String base;
+	private final String imageBase;
+	private final String settings;
+	private final IModelFactory factory;
+	final String urlScheme;
+
+	AbstractMakeTask(final String source, final String base, final String imageBase, final String settings, final String urlScheme, final IModelFactory factory)
+	{
+		this.source = source;
+		this.base = base;
+		this.imageBase = imageBase;
+		this.settings = settings;
+		this.factory = factory;
+		this.urlScheme = urlScheme;
+	}
+
+	@Override
+	protected Model doInBackground(final Void... args)
+	{
+		try
+		{
+			return this.factory.make(this.source, this.base, this.imageBase, this.settings);
+		}
+		catch (final Exception e)
+		{
+			Log.e(TAG, "Error making model", e);
+		}
+		return null;
+	}
+}
