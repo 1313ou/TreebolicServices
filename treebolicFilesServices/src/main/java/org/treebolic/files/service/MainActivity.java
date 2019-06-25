ab15736f3da4a1d5c7f82154da45aa1685b1e38d
bbou <bbou@ac-toulouse.fr>
2019-06-25 19:30:38 +0200

    alertdialog balancing

--- treebolicFilesServices/src/main/java/org/treebolic/files/service/MainActivity.java
+++ treebolicFilesServices/src/main/java/org/treebolic/files/service/MainActivity.java
@@ -5,7 +5,6 @@
 package org.treebolic.files.service;
 
 import android.annotation.SuppressLint;
-import android.app.AlertDialog;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
@@ -46,6 +45,7 @@ import java.io.File;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
