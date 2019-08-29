e4708912a116429fd23de1ed53870bd8ac66e06e
bbou <bbou@ac-toulouse.fr>
2019-08-29 15:30:26 +0200

    inferred nullity remove settings lib

--- treebolicWordNetServices/src/main/java/org/treebolic/wordnet/service/SettingsActivity.java
+++ treebolicWordNetServices/src/main/java/org/treebolic/wordnet/service/SettingsActivity.java
@@ -4,16 +4,12 @@
 
 package org.treebolic.wordnet.service;
 
-import android.content.Context;
-import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.MenuItem;
 
 import org.treebolic.AppCompatCommonPreferenceActivity;
 import org.treebolic.TreebolicIface;
 import org.treebolic.preference.OpenEditTextPreference;
-
-import java.util.List;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
@@ -22,78 +18,14 @@ import androidx.core.app.NavUtils;
 import androidx.preference.EditTextPreference;
 import androidx.preference.ListPreference;
 import androidx.preference.PreferenceFragmentCompat;
-import androidx.preference.legacy.contrib.Header;
 
 /**
- * A AppCompatPreferenceActivity that presents a set of application settings. On handset devices, settings are presented as a single list. On tablets, settings
- * are split by category, with category headers shown to the left of the list of settings.
- * <p>
- * See <a href="http://developer.android.com/design/patterns/settings.html"> Android Design: Settings</a> for design guidelines and the <a
- * href="http://developer.android.com/guide/topics/ui/settings.html">Settings API Guide</a> for more information on developing a Settings UI.
+ * Settings activity
+ *
+ * @author Bernard Bou
  */
 public class SettingsActivity extends AppCompatCommonPreferenceActivity
 {
-	// E V E N T S
-
-	@Override
-	protected void onCreate(final Bundle savedInstanceState)
-	{
-		// super
-		super.onCreate(savedInstanceState);
-
-		// toolbar
-		final Toolbar toolbar = findViewById(R.id.toolbar);
-		setSupportActionBar(toolbar);
-
-		// set up the action bar
-		final ActionBar actionBar = getSupportActionBar();
-		if (actionBar != null)
-		{
-			actionBar.setDisplayOptions(ActionBar.DISPLAY_USE_LOGO | ActionBar.DISPLAY_SHOW_TITLE | ActionBar.DISPLAY_SHOW_HOME | ActionBar.DISPLAY_HOME_AS_UP);
-		}
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(@NonNull final MenuItem item)
-	{
-		if (item.getItemId() == android.R.id.home)
-		{
-			NavUtils.navigateUpFromSameTask(this);
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
-
-	// S E T U P
-
-	@Override
-	public void onBuildHeaders(@NonNull final List<Header> target)
-	{
-		loadHeadersFromResource(R.xml.pref_headers, target);
-	}
-
-	// D E T E C T I O N
-
-	@Override
-	public boolean isValidFragment(final String fragmentName)
-	{
-		return GeneralPreferenceFragment.class.getName().equals(fragmentName) || ServicePreferenceFragment.class.getName().equals(fragmentName);
-	}
-
-	@Override
-	public boolean onIsMultiPane()
-	{
-		return SettingsActivity.isLargeTablet(this);
-	}
-
-	/**
-	 * Helper method to determine if the device has an extra-large screen. For example, 10" tablets are extra-large.
-	 */
-	private static boolean isLargeTablet(@NonNull final Context context)
-	{
-		return (context.getResources().getConfiguration().screenLayout & Configuration.SCREENLAYOUT_SIZE_MASK) >= Configuration.SCREENLAYOUT_SIZE_LARGE;
-	}
-
 	// F R A G M E N T S
 
 	public static class GeneralPreferenceFragment extends PreferenceFragmentCompat
