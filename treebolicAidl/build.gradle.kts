cd9ee6724185480c3d67db66d9e56204c1bedd87
1313ou <1313ou@gmail.com>
2025-06-15 16:21:09 +0200

    to kts

---
+++ treebolicAidl/build.gradle.kts
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) Treebolic 2019. Bernard Bou <1313ou@gmail.com>
+ */
+
+plugins {
+    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.kotlinAndroid)
+}
+
+private val vCompileSdk by lazy { rootProject.extra["compileSdk"] as Int }
+private val vMinSdk by lazy { rootProject.extra["minSdk"] as Int }
+
+android {
+
+    namespace = "org.treebolic.services.iface.aidl"
+
+    compileSdk = vCompileSdk
+
+    defaultConfig {
+        minSdk = vMinSdk
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        multiDexEnabled = true
+    }
+
+    aidlPackagedList("org/treebolic/services/iface/ITreebolicAIDLService.aidl")
+
+    buildFeatures {
+        aidl = true
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    implementation(libs.core.ktx)
+}
+
---
+++ treebolicClientsLib/build.gradle.kts
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) Treebolic 2019. Bernard Bou <1313ou@gmail.com>
+ */
+
+plugins {
+    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.kotlinAndroid)
+}
+
+private val vCompileSdk
+        by lazy { rootProject.extra["compileSdk"] as Int }
+private val vMinSdk
+        by lazy { rootProject.extra["minSdk"] as Int }
+
+android {
+
+    namespace = "org.treebolic.clients"
+
+    compileSdk = vCompileSdk
+
+    defaultConfig {
+        minSdk = vMinSdk
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        multiDexEnabled = true
+    }
+
+    compileOptions {
+        isCoreLibraryDesugaringEnabled = true
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    implementation(libs.treebolic.model)
+
+    implementation(project(":treebolicAidl")) // needed
+    implementation(project(":treebolicServicesIface"))
+    implementation(project(":treebolicClientsIface"))
+    implementation(project(":treebolicIface"))
+    implementation(project(":treebolicParcel"))
+    implementation(project(":commonLib"))
+
+    implementation(libs.appcompat)
+    implementation(libs.annotation)
+
+    implementation(libs.core.ktx)
+    coreLibraryDesugaring(libs.desugar)
+}
